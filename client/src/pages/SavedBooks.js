4efdd0da123bc0cc0bd2584bbbab24d91accca6c
AjaniBrowne <ajani.browne@yahoo.com>
2022-08-29 20:39:03 -0400

    final touches
    fixed errors for developing page

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
-import { useQuery, useMutation } from "@apollo/react-hooks";
+import { removeBookId } from "../utils/localStorage";
+import { useQuery, useMutation } from'@apollo/client'  ;
 import { GET_ME } from "../utils/queries";
 import { REMOVE_BOOK } from "../utils/mutations";
 
 const SavedBooks = () => {
   // const [userData, setUserData] = useState({});
   const { loading, data } = useQuery(GET_ME);
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const [removeBook] = useMutation(REMOVE_BOOK);
   const userData = data?.me || {};
 
   // use this to determine if `useEffect()` hook needs to run again
@@ -76,7 +74,7 @@ const SavedBooks = () => {
     //   const updatedUser = await response.json();
     //   setUserData(updatedUser);
     //   // upon success, remove book's id from localStorage
-    //   removeBookId(bookId);
+       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
